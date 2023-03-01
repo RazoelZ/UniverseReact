6fe3ab7823ffbe21a9405ad9e9a352b0d81c7b34
RazoelZ <danydarmawannn@gmail.com>
2023-03-01 22:58:23 +0700

    First commit

---
+++ src/components/intro.js
@@ -0,0 +1,10 @@
+function Intro(props){
+    return(
+        <div>
+            <h1>Intro</h1>
+            <p>Halo nama saya adalah : {props.name}</p>
+        </div>
+    )
+}
+
+export default Intro;
---
+++ src/components/profile.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Profile extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Profile</h2>
+        <p>Belajar React App</p>
+      </div>
+    );
+  }
+}
+
+export default Profile;
